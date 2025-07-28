4a336636c87bf4810d4e7bd9d5d185f216f2a0f1
ntp-k <natthaphat41@gmail.com>
2025-07-28 21:11:36 +0700

    bugfix openapi version setting

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from app.cart import cart_router
 from app.catalog import catalog_router
 from app.product import product_router
 from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI(openapi_version="3.0.3", docs_url=None)
+app = FastAPI(docs_url=None)
 
 @app.get("/")
 def root():
@@ -17,7 +18,7 @@ def custom_swagger_ui():
 @app.get("/swagger.json", include_in_schema=False)
 def openapi_spec():
     from fastapi.openapi.utils import get_openapi
-    return get_openapi(title="Retail Store API", version="1.0", routes=app.routes)
+    return get_openapi(title="Retail Store API", version="3.0.3", routes=app.routes)
 
 app.include_router(cart_router)
 app.include_router(catalog_router)
